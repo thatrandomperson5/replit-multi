e2e148ce179e8ade201cb1fe8a52d5ea1016f08d
thatrandomperson5 <94306853+thatrandomperson5@users.noreply.github.com>
2022-06-29 19:24:13 -0400

    Update setuptools.sh

--- clone/setuptools.sh
+++ clone/setuptools.sh
@@ -1,5 +1,5 @@
 RT=$( pwd; );
 echo "export PATH=$PATH:$RT/multi-tools/;";
-echo "chmod u+x ./multi-tools/.multi;";
+
 echo "chmod u+x ./multi-tools/multi.bin;";
 echo "chmod u+x ./multi-tools/multi;";
